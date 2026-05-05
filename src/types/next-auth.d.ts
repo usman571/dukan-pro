9bad894ec75264af6a362053c2564267c69336dc
usman571 <developer2119181@gmail.com>
2026-05-05 14:58:35 +0500

    feat(auth): Add shopName to NextAuth session and JWT types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,11 +5,13 @@ declare module 'next-auth' {
     user: {
       id: string;
       role: string;
+      shopName?: string;
     } & DefaultSession['user'];
   }
 
   interface User {
     role?: string;
+    shopName?: string;
   }
 }
 
@@ -17,5 +19,6 @@ declare module 'next-auth/jwt' {
   interface JWT {
     id: string;
     role: string;
+    shopName?: string;
   }
 }
